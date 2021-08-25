2ae58881062d5ffe724adfc04be5149d5d8c7e22
Elokeet <elokeet@gmail.com>
2021-08-25 13:26:49 +0200

    Added changelog

--- main.js
+++ main.js
@@ -1,41 +1,91 @@
-var numClicks = 0;
-var automatics = 0;
-var autoCost = 2;
+var copies = 1;
+var strand = "";
+var growthCost = 2;
+var copyRate = 1;
 
-function clicked(number){
-	numClicks = numClicks + number;
-	document.getElementById("clicks").innerHTML = numClicks;
+var showCopiesText = false;
+var showStrandText = false;
+
+function addBase(){
+	if (showStrandText == false) {
+		showStrandText = true;
+		document.getElementById("state").innerHTML = "You are a strand of RNA.";
+		document.getElementById("strandText").style.display="block";
+	}
+	rand = Math.floor(Math.random() * 4);
+	if (rand == 0) {
+		strand += "C";
+	} else if (rand == 1) {
+		strand += "G";
+	} else if (rand == 2) {
+		strand += "A";
+	} else {
+		strand += "U";
+	};
+	document.getElementById("strand").innerHTML = strand;
 };
 
-function automate(){
-	if (numClicks >= autoCost){
-		automatics = automatics + 1;
-		numClicks = numClicks - autoCost;
-		autoCost = autoCost + 1;
-		document.getElementById("clicks").innerHTML = numClicks;
-		document.getElementById("automatics").innerHTML = automatics;
-		document.getElementById("autoCost").innerHTML = autoCost;
+function clicked(number){
+	if (showCopiesText == false) {
+		showCopiesText = true;
+		document.getElementById("copiesText").style.display="block";
+		document.getElementById("growthText").style.display="block";
+	};
+	copies = copies + number;
+	document.getElementById("copies").innerHTML = copies;
+};
+
+function grow(){
+	if (copies >= growthCost){
+		addBase();
+		copies = 1;
+		growthCost = growthCost + 1;
+		document.getElementById("copies").innerHTML = copies;
+		document.getElementById("growthCost").innerHTML = growthCost;
 	};
 };
 
-
 function save(){
 	var save = {
-		numClicks: numClicks,
-		automatics: automatics,
-		autoCost: autoCost
+		copies: copies,
+		strand: strand,
+		growthCost: growthCost,
+		copyRate: copyRate,
+		showCopiesText: showCopiesText,
+		showStrandText: showStrandText
 	};
 	localStorage.setItem("save",JSON.stringify(save));
 };
 
 function load(){
 	var savegame = JSON.parse(localStorage.getItem("save"));
-	if (typeof savegame.numClicks !== "undefined") numClicks = savegame.numClicks;
-	if (typeof savegame.automatics !== "undefined") automatics = savegame.automatics;
-	if (typeof savegame.autoCost !== "undefined") autoCost = savegame.autoCost;
-	document.getElementById("clicks").innerHTML = numClicks;
-	document.getElementById("automatics").innerHTML = automatics;
-	document.getElementById("autoCost").innerHTML = autoCost;
+	
+	if (typeof savegame.copies !== "undefined") copies = savegame.copies;
+	if (typeof savegame.strand !== "undefined") strand = savegame.strand;
+	if (typeof savegame.growthCost !== "undefined") growthCost = savegame.growthCost;
+	if (typeof savegame.copyRate !== "undefined") copyRate = savegame.copyRate;
+	if (typeof savegame.showCopiesText !== "undefined") showCopiesText = savegame.showCopiesText;
+	if (typeof savegame.showStrandText !== "undefined") showStrandText = savegame.showStrandText;
+	
+	document.getElementById("copies").innerHTML = copies;
+	document.getElementById("strand").innerHTML = strand;
+	document.getElementById("growthCost").innerHTML = growthCost;
+	
+	if (showCopiesText == false) {
+		document.getElementById("copiesText").style.display="none";
+		document.getElementById("growthText").style.display="none";
+	} else {
+		document.getElementById("copiesText").style.display="block";
+		document.getElementById("growthText").style.display="block";
+	};
+	if (showStrandText == false) {
+		document.getElementById("strandText").style.display="none";
+		document.getElementById("state").innerHTML = "You are a single nucleotide.";
+	} else {
+		document.getElementById("strandText").style.display="block";
+		document.getElementById("state").innerHTML = "You are a strand of RNA.";
+	};
+	
 };
 
 function deleteSave(){
@@ -43,5 +93,7 @@ function deleteSave(){
 };
 
 window.setInterval(function(){
-	clicked(automatics)
+	if (copyRate > 0) {
+		clicked(copyRate);
+	};
 }, 1000);
