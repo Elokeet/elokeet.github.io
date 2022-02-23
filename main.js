a32a5f9d9b9f6c41243a9daa7832c6d9752a0743
Elokeet <elokeet@gmail.com>
2022-02-23 21:15:08 +0100

    Now it's a dragon's life. Added eating mechanic.

--- main.js
+++ main.js
@@ -1,58 +1,57 @@
 var i = 0;
 var n = 0;
-var seeds = 0;
-var pecking = false;
+var sheep = 0;
+var hunting = false;
 var building = false;
-var seedWidth = 1
+var huntWidth = 1
 var nestWidth = 1
-function startPecking(){
-	pecking = true;
+function startHunting(){
+	hunting = true;
 	building = false;
-	var elem = document.getElementById("seedBar");
-    var id = setInterval(frame, 20);
+	var elem = document.getElementById("huntBar");
+    var id = setInterval(frame, 10);
     function frame() {
-	    if (pecking == false) {
+	    if (hunting == false) {
 			clearInterval(id);
-		}
-		else if (seedWidth >= 100) {
-			seedWidth = 0;
-			elem.style.seedWidth = seedWidth + "%";
-			seeds += 1;
-			document.getElementById("seeds").innerHTML = seeds;
+		} else if (huntWidth >= 100) {
+			huntWidth = 0;
+			elem.style.huntWidth = huntWidth + "%";
+			sheep += 1;
+			document.getElementById("sheep").innerHTML = sheep;
 		} else {
-			seedWidth++;
-			elem.style.width = seedWidth + "%";
+			huntWidth++;
+			elem.style.width = huntWidth + "%";
 		}
-		document.getElementById("test").innerHTML = seedWidth;
+		//document.getElementById("test").innerHTML = huntWidth;
     }
 }
 function startBuilding(){
-	pecking = false;
+	hunting = false;
 	building = true;
 	var elem = document.getElementById("nestBar");
     var id = setInterval(frame, 80);
     function frame() {
 	    if (building == false || nestWidth >= 100) {
 			clearInterval(id);
-		} else {
+		} else if (sheep > 0) {
 			nestWidth++;
 			elem.style.width = nestWidth + "%";
 		}
-		document.getElementById("test").innerHTML = nestWidth;
+		//document.getElementById("test").innerHTML = nestWidth;
     }
 }
-function peckSeeds(interval) {
+function pecksheep(interval) {
   if (i == 0) {
     i = 1;
-    var elem = document.getElementById("seedBar");
+    var elem = document.getElementById("huntBar");
     var width = 1;
     var id = setInterval(frame, interval);
     function frame() {
-      if (width >= 100 || pecking == false) {
+      if (width >= 100 || hunting == false) {
         clearInterval(id);
         i = 0;
-		seeds += 1;
-		document.getElementById("seeds").innerHTML = seeds;
+		sheep += 1;
+		document.getElementById("sheep").innerHTML = sheep;
       } else {
         width++;
         elem.style.width = width + "%";
@@ -72,114 +71,46 @@ function buildNest(interval) {
       } else {
         width++;
 		elem.style.width = width + "%";
-      } 
+      }
     }
   }
 }
 window.setInterval(function(){
-	/*if (pecking) {
-		peckSeeds(20);
+	sheep -= 1;
+	document.getElementById("sheep").innerHTML = sheep;
+	if (sheep <= 0) {
+		document.getElementById("tips").innerHTML = ("You are low on sheep. You're" +
+		 																						 " not going to be able to do " +
+																								 "much else until you get something to eat.");
+	} else if (nestWidth >= 100) {
+	  document.getElementById("tips").innerHTML = "You've built a nest! Now you have somewhere to sleep.";
+	} else {
+		document.getElementById("tips").innerHTML = "Every dragon needs a nest.";
+	}
+	/*if (hunting) {
+		pecksheep(20);
 	}
 	if (building) {
 		buildNest(80);
 	}*/
-}, 1000);
-
-/*var copies = 1;
-var strand = "";
-var growthCost = 2;
-var copyRate = 0;
-var ribozymeCost = 3;
-var ribozymes = 0;
-const round18 = "GGAAAAAGACAAAUCUGCCCUCAGAGCUUGAGAACAUCUUCGGAUGCAGAGGAGGCAGCCUCCGGUGGCGCGAUAGCGCCAACGUUCUCAACAGGCGCCCAAUACUCCCGCUUCGGCGGGUGGGGAUAACACCUGACGAAAAGGCGAUGUUAGACACGCCAAGGUCAUAAUCCCCGGAGCUUCGGCUCC";
-
-var showCopiesText = false;
-var showStrandText = false;
-var showRibozymeText = false;
-
-function addBase(){
-	if (showStrandText == false) {
-		showStrandText = true;
-		document.getElementById("state").innerHTML = "You are a strand of RNA.";
-		document.getElementById("strandText").style.display="block";
-	};
-	
-	if (strand.length < round18.length) {
-		strand += round18.charAt(strand.length);
-		if (strand.length == round18.length) {
-			round18Created();
-		};
-	};
-	/*
-	if (strand.length == 2) {
-		showRibozymeText = true;
-		document.getElementById("ribozymeText").style.display="block";
-	};
-	
-	rand = Math.floor(Math.random() * 4);
-	if (rand == 0) {
-		strand += "C";
-	} else if (rand == 1) {
-		strand += "G";
-	} else if (rand == 2) {
-		strand += "A";
-	} else {
-		strand += "U";
-	};
-	
-	document.getElementById("strand").innerHTML = strand;
-};
-
-function round18Created() {
-	document.getElementById("round18Text").style.display="block";
-};
-
-function addRibozyme() {
-	if (copies >= ribozymeCost) {
-		copies = copies - ribozymeCost;
-		copyRate = copyRate + 1;
-		ribozymes = ribozymes + 1;
-		ribozymeCost = ribozymeCost + 3;
-		document.getElementById("copies").innerHTML = copies;
-		document.getElementById("ribozymeCost").innerHTML = ribozymeCost;
-		document.getElementById("ribozymes").innerHTML = ribozymes;
-	};
-};
-
-function clicked(number){
-	if (showCopiesText == false) {
-		showCopiesText = true;
-		document.getElementById("copiesText").style.display="block";
-		document.getElementById("growthText").style.display="block";
-	};
-	copies = copies + number;
-	document.getElementById("copies").innerHTML = copies;
-};
-
-function grow(){
-	if (copies >= growthCost){
-		addBase();
-		copies = 1;
-		growthCost = growthCost + 1;
-		document.getElementById("copies").innerHTML = copies;
-		document.getElementById("growthCost").innerHTML = growthCost;
-	};
-};
-*/
+}, 3000);
 
 function save(){
 	var save = {
-		seeds: seeds
+		sheep: sheep,
+		nestWidth: nestWidth
 	};
 	localStorage.setItem("save",JSON.stringify(save));
 };
 
 function load(){
 	var savegame = JSON.parse(localStorage.getItem("save"));
-	
-	if (typeof savegame.seeds !== "undefined") seeds = savegame.seeds;
-	
-	document.getElementById("seeds").innerHTML = seeds;	
+
+	if (typeof savegame.sheep !== "undefined") sheep = savegame.sheep;
+  if (typeof savegame.nestWidth !== "undefined") nestWidth = savegame.nestWidth;
+
+	document.getElementById("sheep").innerHTML = sheep;
+	document.getElementById("nestBar").style.width = nestWidth + "%";
 };
 
 function deleteSave(){
