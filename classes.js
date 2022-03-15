79e9693753a20525dc9dd49a069656284a45feff
Elokeet <elokeet@gmail.com>
2022-03-15 19:02:19 +0100

    Added searching for mate and mating. Better saving.

--- classes.js
+++ classes.js
@@ -1,10 +1,29 @@
 class Skill {
-    constructor(startingSpeed) {
-        this.level = 0;
-        this.exp = 0;
-        this.maxexp = 100;
-        this.speed = startingSpeed;
+    constructor(startingSpeed, level = 0, exp = 0, maxexp = 100, bonus = 1) {
+        this.level = level;
+        this.exp = exp;
+        this.maxexp = maxexp;
+        this.generationBonus = bonus;
+        this.speed = startingSpeed*this.generationBonus*Math.pow(1.05, this.level);
+        this.startingSpeed = startingSpeed;
     }
+
+    toJSON() {
+        return {
+                $type: 'classes.Skill',
+                level: this.level,
+                exp: this.exp,
+                maxexp: this.maxexp,
+                generationBonus: this.generationBonus,
+                startingSpeed: this.startingSpeed
+        };
+    }
+
+    static fromJSON(data) {
+        return new Skill(data.startingSpeed, data.level, data.exp, data.maxexp,
+            data.generationBonus);
+    }
+
     incExp() {
         this.exp += 1;
         if (this.exp >= this.maxexp) {
--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-var i = 0;
-var n = 0;
 var sheep = 0;
-var hunting = false;
-var building = false;
 var huntWidth = 0;
 var nestWidth = 0;
 var searchWidth = 0;
 var currBar;
 var activity;
+var found = 0;
 huntLevel = new Skill(7);
-buildLevel = new Skill(0.5);
-searchLevel = new Skill(1);
+buildLevel = new Skill(0.2);
+searchLevel = new Skill(0.5);
 
 function startHunting(){
 	activity = "hunting"
@@ -32,6 +29,27 @@ function startBuilding(){
 	}
 }
 
+function mate() {
+	if (searchWidth >= 100 && found == 1) {
+		sheep = 0;
+		huntWidth = 0;
+		nestWidth = 0;
+		searchWidth = 0;
+		activity = "";
+		found = 0;
+		huntLevel = new Skill(7, 0, 0, 100, 1.1);
+		buildLevel = new Skill(0.2, 0, 0, 100, 1.1);
+		searchLevel = new Skill(0.5, 0, 0, 100, 1.1);
+		document.getElementById("sheep").innerHTML = sheep;
+		document.getElementById("huntBar").style.width = huntWidth + "%";
+		document.getElementById("searchBar").style.width = searchWidth + "%";
+		document.getElementById("nestBar").style.width = nestWidth + "%";
+		document.getElementById("huntLevel").innerHTML = huntLevel.level;
+		document.getElementById("searchLevel").innerHTML = searchLevel.level;
+		document.getElementById("buildLevel").innerHTML = buildLevel.level;
+	}
+}
+
 function updateBar() {
 	if (activity == "hunting") {
 		huntWidth += huntLevel.speed;
@@ -46,21 +64,17 @@ function updateBar() {
 			currBar.style.width = huntWidth + "%";
 		}
 	}
-	if (activity == "searching") {
+	if (activity == "searching" && searchWidth < 100 && sheep > 0) {
 		searchLevel.incExp();
 		currLevel.innerHTML = searchLevel.level;
-		if (searchWidth < 100 && sheep > 0) {
-			searchWidth += searchLevel.speed;
-			currBar.style.width = Math.min(searchWidth, 100) + "%";
-		}
+		searchWidth += searchLevel.speed;
+		currBar.style.width = Math.min(searchWidth, 100) + "%";
 	}
-	if (activity == "building") {
+	if (activity == "building" && nestWidth < 100 && sheep > 0) {
 		buildLevel.incExp();
 		currLevel.innerHTML = buildLevel.level;
-		if (nestWidth < 100 && sheep > 0) {
-			nestWidth += buildLevel.speed;
-			currBar.style.width = Math.min(nestWidth, 100) + "%";
-		}
+		nestWidth += buildLevel.speed;
+		currBar.style.width = Math.min(nestWidth, 100) + "%";
 	}
 };
 
@@ -69,31 +83,45 @@ function update(){
 	sheep -= 1/20;
 	if (sheep <= 0) {
 		document.getElementById("sheep").innerHTML = 0;
-		document.getElementById("tips").innerHTML = ("You are low on sheep. You're" +
+		document.getElementById("tips").innerHTML = ("You're starving. You're" +
 		" not going to be able to do " +
 		"much else until you get something to eat.");
 	} else {
 		document.getElementById("sheep").innerHTML = Math.round(sheep);
-		if (searchWidth < 100) {
+		if (searchWidth < 100 && found == 0) {
 			document.getElementById("tips").innerHTML = ("Every dragon needs a " +
 			"nest. Let's start by looking for a good spot.");
 		} else if (nestWidth < 100) {
 			document.getElementById("tips").innerHTML = ("You've found a good " +
 			"spot for the nest. Now let's build it.");
 		}
-		else {
+		else if (nestWidth >= 100 && searchWidth >= 100 && found == 0) {
 			document.getElementById("tips").innerHTML = ("You've built a nest! " +
-			"Now you have somewhere to sleep.");
+			"Now you have somewhere to sleep. Now let's search for a mate.");
+			searchWidth = 0;
+			document.getElementById("searchBar").style.width = searchWidth + "%";
+			found += 1;
+		}
+		else if (searchWidth >= 100 && found == 1) {
+			document.getElementById("tips").innerHTML = ("You found a mate! " +
+			"Mate to move on to a new generation.");
+			document.getElementById("mate").style.display = "block";
 		}
 	}
 };
 
 window.setInterval(update, 50);
+window.setInterval(save, 3000)
 
 function save(){
 	var save = {
 		sheep: sheep,
-		nestWidth: nestWidth
+		nestWidth: nestWidth,
+		searchWidth: searchWidth,
+		huntLevel: huntLevel,
+		searchLevel: searchLevel,
+		buildLevel: buildLevel,
+		found: found
 	};
 	localStorage.setItem("save",JSON.stringify(save));
 };
@@ -103,9 +131,22 @@ function load(){
 
 	if (typeof savegame.sheep !== "undefined") sheep = savegame.sheep;
 	if (typeof savegame.nestWidth !== "undefined") nestWidth = savegame.nestWidth;
+	if (typeof savegame.searchWidth !== "undefined") searchWidth = savegame.searchWidth;
+	if (typeof savegame.huntLevel !== "undefined") huntLevel =
+		Skill.fromJSON(savegame.huntLevel);
+	if (typeof savegame.searchLevel !== "undefined") searchLevel =
+		Skill.fromJSON(savegame.searchLevel);
+	if (typeof savegame.buildLevel !== "undefined") buildLevel =
+		Skill.fromJSON(savegame.buildLevel);
+	if (typeof savegame.found !== "undefined") found = savegame.found;
 
 	document.getElementById("sheep").innerHTML = sheep;
 	document.getElementById("nestBar").style.width = nestWidth + "%";
+	document.getElementById("searchBar").style.width = searchWidth + "%";
+	document.getElementById("huntLevel").innerHTML = huntLevel.level;
+	document.getElementById("searchLevel").innerHTML = searchLevel.level;
+	document.getElementById("buildLevel").innerHTML = buildLevel.level;
+
 };
 
 function deleteSave(){
